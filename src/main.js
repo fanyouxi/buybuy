18a5db09b3290f71c2de5381511defb79210d410
fyx <228197155@qq.com769539716>
2018-11-22 20:22:20 +0800

    blackmaimai首页和右边的渲染出来了

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 引入全局样式
+import './assets/site/css/style.css'
+//导入路由
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+//写组件
+ import index from './components/index.vue'
+
+//写路由规则
+const routes =[
+  {path:'/',redirect:'/index'},
+  {path:'/index',component:index},
+
+]
+
+//初始化VueRouter
+let router = new VueRouter({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
